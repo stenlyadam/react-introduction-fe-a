911fd1a608075541697d560a4090000ee7a85f75
Stenly Adam <stenly.adam@unklab.ac.id>
2025-11-04 09:59:56 +0800

    event handler & state

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
 //Component adalah function yg return JSX
 //JSX hanya boleh memiliki 1 parent element
+import React, { useState } from "react";
 import HelloWorld from "./HelloWorld";
 
 const App = () => {
-  let hello = "Hello World";
+  // let hello = "Hello World"; // Ini variabel biasa
+  const [hello, setHello] = useState("Hello World!!!"); //Ini variabel state
+
+  const handleClick = () => {
+    console.log("button di klik");
+    // hello = "Selamat Datang"; //ini cara merubah variabel biasa
+    setHello("Selamat Datang"); //ini cara merubah variabel state
+    console.log(hello);
+  };
   return (
     <>
       <div className="text-[50px] bg-blue-500 m-10 p-10 font-serif">
         {hello}
       </div>
-      <HelloWorld text="This is a child component 1" />
-      <HelloWorld text="This is a child component 2" />
-      <HelloWorld text="This is a child component 3" />
+      <HelloWorld
+        text="This is a child component 1"
+        handleClick={handleClick}
+      />
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={handleClick}
+      >
+        Click Me
+      </button>
     </>
   );
 };
--- src/HelloWorld/index.jsx
+++ src/HelloWorld/index.jsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
 import HelloMoon from "../HelloMoon";
 //Tipe data props adalah Object
 
-const HelloWorld = ({ text }) => {
+const HelloWorld = ({ text, handleClick }) => {
+  const [welcome, setWelcome] = useState("Welcome");
+  const handleClickChild = () => {
+    setWelcome("Selamat Datang");
+  };
   return (
     <>
       <div className="text-[25px] bg-red-500 m-10 p-10 font-serif">
         {text}
+        {welcome}
         <HelloMoon />
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={handleClick}
+        >
+          Click Me
+        </button>
+        <button
+          className="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={handleClickChild}
+        >
+          Click Me
+        </button>
       </div>
     </>
   );
